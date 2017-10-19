8c3c01c6c34a2cbcfe4d69eb1a3a2ab6ce857e1d
Stefano Castello <steisbetter@outlook.it>
2017-10-19 09:22:09 +0200

    1.0.1

--- MyAttribute/MyAtt.cs
+++ MyAttribute/MyAtt.cs
@@ -10,6 +10,21 @@ namespace MyAttribute
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
     public class ExecuteMe : Attribute
     {
-        
+        private int v { get; }
+        private string v1 { get; }
+        private string v2 { get; }
+
+        public ExecuteMe() { }
+
+        public ExecuteMe(int v)
+        {
+            this.v = v;
+        }
+
+        public ExecuteMe(string v1, string v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
     }
 }

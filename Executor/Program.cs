63f8977623b1f359a7a201c4ff4ba0817d48fcad
Stefano Castello <steisbetter@outlook.it>
2017-10-20 11:11:24 +0200

    Invoke Correct Todo M3 and Attribute constructor param

--- Executor/Program.cs
+++ Executor/Program.cs
@@ -19,6 +19,23 @@ namespace Executor
                                 {
                                     Console.WriteLine(meth.AttributeType);
                                     //TODO Invoke method
+                                    object result = null;
+                                    ParameterInfo[] parameters = method.GetParameters();
+                                    object classInstance = Activator.CreateInstance(type, null);
+
+                                    switch (parameters.Length)
+                                    {
+                                        case 0:
+                                            result = method.Invoke(classInstance, null);
+                                            break;
+                                        case 1:
+                                            object[] parametersArray = new object[] {1};         
+                                            result = method.Invoke(classInstance, parametersArray);
+                                            break;
+                                        case 2:
+                                            
+                                            break;
+                                    }
                                 }
                                 
                     //Console.WriteLine(type.FullName);
